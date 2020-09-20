60729d507652cb9d57adae9c8434a9703addfb76
cboswel1 <christianboswell86@gmail.com>
2020-09-20 11:42:34 -0600

    inital commit

---
+++ Develop/index.js
@@ -0,0 +1,61 @@
+// array of questions for user
+const questions = [
+    {
+      type: "input",
+      message: "What is the title of your project?",
+      name: "title"
+    },
+    {
+      type: "input",
+      message: "Description of your project:",
+      name: "description"
+    },
+    {
+      type: "input",
+      message: "Table of Contents:",
+      name: "contents"
+    }, 
+    {
+      type: "input",
+      message: "Installation instructions: ",
+      name: "installation"
+    }, 
+    {
+      type: "input",
+      message: "Usage instructions: ",
+      name: "usage"
+    }, 
+    {
+      type: "input",
+      message: "License: ",
+      name: "license"
+    },
+    {
+      type: "input",
+      message: "Contributing: ",
+      name: "contributing"
+    },
+    {
+      type: "input",
+      message: "Tests: ",
+      name: "tests"
+    },
+    {
+      type: "input",
+      message: "Questions: ",
+      name: "questions"
+    }
+];
+
+console.log(questions);
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
